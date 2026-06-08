71d6a1ad8feef684c36bf92a88e817b03deccfbf
Nikesh Lama <nikeshlama76@gmail.com>
2026-06-08 12:15:48 -0400

    vite config integrated

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/room-service-menu/', // <-- MUST have both slashes and match repository name exactly
 })
