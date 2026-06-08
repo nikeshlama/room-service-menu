44305418b86050440e8a3b1c37f17f6318f7504d
Nikesh Lama <nikeshlama76@gmail.com>
2026-06-08 12:38:49 -0400

    done

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/room-service-menu/', // <-- MUST have both slashes and match repository name exactly
+  base: './', // FIXED: Changing this to './' forces relative paths for all assets, ignoring subfolder issues completely
 })
